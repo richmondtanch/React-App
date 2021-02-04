9f819fdc3b612632560dc3ce74898ee6d171b480
Richmond Tan <richmondtanch@gmail.com>
2021-02-04 21:52:47 +0800

    render as list

--- src/App.js
+++ src/App.js
@@ -35,11 +35,17 @@ class App extends Component {
     console.log("namechange")
     this.setState( {
       persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
+        { id:'qwee', name: 'Max', age: 28 },
+        { id:'qwe2e',name: event.target.value, age: 29 },
+        { id:'qwe4e',name: 'Stephanie', age: 26 }
       ]
     })
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons:persons});
   }
 
   render() {
@@ -49,22 +55,13 @@ class App extends Component {
     if (this.state.showPerson) {
       persons = (
         <div >
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-          />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this,"Jack")}
-            changed={this.nameChangeHandler}
-          >
-            My Hobbies: Racing
-          </Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}
-          />
+          {this.state.persons.map((person, index) => {
+            return <Person name={person.name}
+              age={person.age}
+              click={() => this.deletePersonHandler(index)}
+              key={person.id}
+                />
+          })}
         </div>
       );
     }
